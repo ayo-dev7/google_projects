b3dc8df5cff1b270762e3d439230ad2c8100c9c8
ayo-dev7 <ayoadebiyi95@gmail.com>
2024-09-25 18:47:31 +0100

    made changes to setup

--- api_services/setup.py
+++ api_services/setup.py
@@ -1,17 +1,23 @@
 from setuptools import setup, find_packages
 import os
 
+# Get the long description from the README file
+try:
+    with open(os.path.join(os.path.dirname(__file__), '../README.md'), 'r') as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = 'A package for Google API services'  # Fallback description
+
 setup(
     name='api-services',  
     version='0.1.0',          
     author='Damilola Adebiyi',  
     author_email='ayoadebiyi95@gmail.com',  
     description='A package for Google API services', 
-    long_description=open('../README.md').read(),  
+    long_description=long_description,  
     long_description_content_type='text/markdown',  
     url='https://github.com/ayo-dev7/google_projects',  
     packages=find_packages(),  # Automatically find and include packages
-    package_dir={'':'api_services'},
     install_requires=[
         'cachetools==5.5.0',
         'google-api-python-client==2.146.0',
